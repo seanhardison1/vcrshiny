10fad3f47ee9e60b978b3de15d1e0800713bb01e
seanhardison1 <seanhardison@gmail.com>
2021-01-19 10:38:55 -0700

    forces rsconnect to update shinyapps.io

--- app.R
+++ app.R
@@ -5,7 +5,7 @@
 pkgload::load_all(export_all = T,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = F)
 # vcrshiny::run_app() # add parameters here (if any)
-rsconnect::deployApp()
+rsconnect::deployApp(forceUpdate = getOption("rsconnect.force.update.apps", TRUE))
 # Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
