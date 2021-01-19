0daf1cd1f13c9ee93c9d86863466d6c021760d36
seanhardison1 <seanhardison@gmail.com>
2021-01-19 10:09:22 -0700

    deploy

--- app.R
+++ app.R
@@ -4,4 +4,8 @@
 
 pkgload::load_all(export_all = T,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = F)
-vcrshiny::run_app() # add parameters here (if any)
+# vcrshiny::run_app() # add parameters here (if any)
+rsconnect::deployApp()
+# Launch the ShinyApp (Do not remove this comment)
+# To deploy, run: rsconnect::deployApp()
+# Or use the blue button on top of this file
