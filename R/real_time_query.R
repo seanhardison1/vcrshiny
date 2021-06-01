e66958f396775e9665cb3fbd44ec4064f8405d5c
seanhardison1 <seanhardison@gmail.com>
2021-06-01 10:18:40 -0400

    real time query

--- R/real_time_query.R
+++ R/real_time_query.R
@@ -20,58 +20,52 @@ real_time_query <- function(){
     dplyr::select(-barometric_pressure)
   
   if (nrow(infile1) != 0){
-  # Process for use in package format
-  tides_new_df <-
-    infile1 %>% 
-    
-    # select data from the Oyster station only
-    filter(station == "OYST") %>% 
-    dplyr::select(-station) %>% 
-    
-    # convert missing values to NA
-    mutate_all(function(x)ifelse(x == ".", NA, x)) %>% 
-    
-    # process the datetimes
-    mutate(date = as.Date(date, format = "%d%b%Y"),
-           time = format(strptime(substr(as.POSIXct(sprintf("%04.0f", as.numeric(time)),
-                                                    format="%H%M"), 12, 16), 
-                                  '%H:%M'), '%I:%M %p'),
-           datetime = as.POSIXct(paste(date, time), 
-                                 format="%Y-%m-%d %I:%M %p"),
-           water_temperature = as.numeric(water_temperature)) %>% 
-    dplyr::select(-date, -time) %>%
-    dplyr::filter(!is.na(datetime)) %>%
-    
-    # Convert to tsibble to fill missing datetimes
-    filter(!duplicated(datetime)) %>% 
-    tsibble(index = datetime) %>% 
-    fill_gaps(.full  = TRUE) %>% 
-    as_tibble() %>%
-    
-    # get the hourly mean
-    group_by(y = year(datetime),
-             m = month(datetime),
-             d = day(datetime),
-             h = hour(datetime)) %>% 
-    dplyr::summarise(relative_tide_level = mean(relative_tide_level, na.rm = T) * 3.28084,
-                     water_temperature = (mean(water_temperature, na.rm = T) * 9/5) + 32) %>% 
-    
-    # convert back to datetime
-    mutate(date = as.Date(paste(y,m,d, sep = "-")),
-           time = strptime(h, "%H"),
-           datetime = ymd_hms(as.POSIXct(paste(date, hour(time)), 
-                                         format="%Y-%m-%d %H"))) %>% 
-    
-    # select variables of interest
-    ungroup() %>% 
-    dplyr::select(datetime, relative_tide_level, water_temperature)
-  
-  tides_new_xts_rt <- xts::xts(x = tides_new_df %>% 
-                                 dplyr::select(-datetime), 
-                               order.by = tides_new_df$datetime)
-  tides_new_xts_rt <- xts::period.apply(tides_new_xts_rt,
-                                      INDEX = xts::endpoints(tides_new_xts_rt, "mins", k=30),
-                                      FUN = mean)
+    # Process for use in package format
+    tides_new_xts_rt <-
+      infile1 %>% 
+      
+      # select data from the Oyster station only
+      dplyr::filter(station == "OYST") %>% 
+      dplyr::select(-station) %>% 
+      
+      # convert missing values to NA
+      dplyr::mutate_all(function(x)ifelse(x == ".", NA, x)) %>% 
+      
+      # process the datetimes
+      dplyr::mutate(date = as.Date(date, format = "%d%b%Y"),
+             time = format(strptime(substr(as.POSIXct(sprintf("%04.0f", as.numeric(time)),
+                                                      format="%H%M"), 12, 16), 
+                                    '%H:%M'), '%I:%M %p'),
+             datetime = as.POSIXct(paste(date, time), 
+                                   format="%Y-%m-%d %I:%M %p"),
+             water_temperature = as.numeric(water_temperature)) %>% 
+      dplyr::select(-date, -time) %>%
+      dplyr::filter(!is.na(datetime)) %>%
+      
+      # Convert to tsibble to fill missing datetimes
+      dplyr::filter(!duplicated(datetime)) %>% 
+      tsibble::tsibble(index = datetime) %>% 
+      tsibble::fill_gaps(.full  = TRUE) %>% 
+      tibble::as_tibble() %>%
+      
+      # get the hourly mean
+      dplyr::group_by(y = lubridate::year(datetime),
+               m = lubridate::month(datetime),
+               d = lubridate::day(datetime),
+               h = lubridate::hour(datetime)) %>% 
+      dplyr::summarise(relative_tide_level = mean(relative_tide_level, na.rm = T) * 3.28084,
+                       water_temperature = (mean(water_temperature, na.rm = T) * 9/5) + 32) %>% 
+      
+      # convert back to datetime
+      dplyr::mutate(date = as.Date(paste(y,m,d, sep = "-")),
+             time = strptime(h, "%H"),
+             datetime = lubridate::ymd_hms(as.POSIXct(paste(date,
+                                                            lubridate::hour(time)), 
+                                           format="%Y-%m-%d %H"))) %>% 
+      
+      # select variables of interest
+      dplyr::ungroup() %>% 
+      dplyr::select(datetime, relative_tide_level, water_temperature)
   } else {
     message("No tidal data available")
     tides_new_xts_rt <- NA
@@ -102,22 +96,22 @@ real_time_query <- function(){
   
   if (nrow(infile2) != 0){
     meteo_new_df <- infile2 %>% 
-      filter(STATION == "OYSM") %>% 
+      dplyr::filter(STATION == "OYSM") %>% 
       dplyr::select(-STATION) %>% 
-      mutate_all(function(x)ifelse(x == ".", NA, x)) %>% 
-      mutate_at(vars(PPT:SOIL.T), as.numeric) %>% 
+      dplyr::mutate_all(function(x)ifelse(x == ".", NA, x)) %>% 
+      dplyr::mutate_at(dplyr::vars(PPT:SOIL.T), as.numeric) %>% 
       tidyr::unite("datetime",c("YEAR", "MONTH", "DAY"), sep = "-", remove = T) %>%
-      mutate(TIME2 = format(strptime(substr(as.POSIXct(sprintf("%04.0f", as.numeric(TIME)),
+      dplyr::mutate(TIME2 = format(strptime(substr(as.POSIXct(sprintf("%04.0f", as.numeric(TIME)),
                                                        format="%H%M"), 12, 16), 
                                      '%H:%M'), '%I:%M %p')) %>% 
-      mutate(datetime = as.POSIXct(paste(.$datetime, TIME2), 
+      dplyr::mutate(datetime = as.POSIXct(paste(.$datetime, TIME2), 
                                    format="%Y-%m-%d %I:%M %p")) %>% 
       dplyr::select(-TIME, -TIME2) %>% 
       dplyr::filter(!is.na(datetime)) %>% 
-      filter(!duplicated(datetime)) %>% 
-      tsibble(index = datetime) %>% 
-      fill_gaps(.full  = TRUE) %>% 
-      as_tibble() %>% 
+      dplyr::filter(!duplicated(datetime)) %>% 
+      tsibble::tsibble(index = datetime) %>% 
+      tsibble::fill_gaps(.full  = TRUE) %>% 
+      tsibble::as_tibble() %>% 
       dplyr::select(datetime, PPT, AVG.T, AVG.WS) %>% 
       dplyr::mutate(AVG.T = (AVG.T * 9/5) + 32,
                     AVG.WS = AVG.WS * 3.28084,
